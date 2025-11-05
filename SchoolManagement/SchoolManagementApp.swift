089739c601f9cee1e913ff0995e991ad5fbbdbde
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2025-11-05 12:47:36 +0530

    Initial Commit

---
+++ SchoolManagement/SchoolManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolManagementApp.swift
+//  SchoolManagement
+//
+//  Created by Sajed Shaikh on 05/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SchoolManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
