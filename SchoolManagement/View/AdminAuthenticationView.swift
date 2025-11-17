7367f0bd7d9325c8acd3a32b8d53deabc09b9e14
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2025-11-17 11:59:40 +0530

    Update authentication view styles for consistency
    Refined text field and button styles in Admin and Student authentication views for improved consistency and better color scheme support. Adjusted font sizes, corner radii, and background/foreground colors to enhance UI appearance in both light and dark modes. Added preferredColorScheme(.light) to previews for consistent preview rendering.

--- SchoolManagement/View/AdminAuthenticationView.swift
+++ SchoolManagement/View/AdminAuthenticationView.swift
@@ -163,32 +163,19 @@ struct AdminAuthenticationTextFieldStyle: TextFieldStyle {
         configuration
             .padding(.horizontal, 20)
             .padding(.vertical, 10)
-            .font(.system(size: 20, weight: .regular, design: .rounded))
-//            .background(
-//                ZStack {
-//                    RoundedRectangle(cornerRadius: 16, style: .continuous)
-//                        .stroke(isFocused.wrappedValue ? Color.black : Color.gray.opacity(0.5), lineWidth: 1)
-//                        .zIndex(1)
-//                    RoundedRectangle(cornerRadius: 16)
-//                        .fill(colorScheme == .light ? Color(.lightGray) : Color(UIColor.darkGray))
-//                        .zIndex(0)
-//                }
-//            )
-        
-        
+            .font(.system(size: 18, weight: .regular, design: .rounded))
             .background(
                 ZStack {
-                    RoundedRectangle(cornerRadius: 16, style: .continuous)
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
                         .stroke(isFocused.wrappedValue ? Color.white : Color.white, lineWidth: 1)
-                        .stroke(Color.black, lineWidth: 1)
+                        .stroke(colorScheme == .light ? Color.black : Color.white, lineWidth: 1)
                         .zIndex(1)
-                    RoundedRectangle(cornerRadius: 16)
+                    RoundedRectangle(cornerRadius: 20)
                         .fill(colorScheme == .light ? Color.white : Color.black)
-                        .stroke(Color.black, lineWidth: 1)
+                        .stroke(colorScheme == .light ? Color.black : Color.white, lineWidth: 1)
                         .zIndex(0)
                 }
             )
-        
             .animation(.easeInOut(duration: 0.2), value: isFocused.wrappedValue )
     }
 }
@@ -197,6 +184,7 @@ struct AdminAuthenticationTextFieldStyle: TextFieldStyle {
     let preview = PersistenceController.preview
     return NavigationStack {
         AdminAuthenticationView()
+            .preferredColorScheme(.light)
             .environmentObject(AdminAuthViewModel(context: preview.container.viewContext))
     }
 }
--- SchoolManagement/View/StudentAuthenticationView.swift
+++ SchoolManagement/View/StudentAuthenticationView.swift
@@ -176,14 +176,16 @@ struct AgreeStyle: ToggleStyle {
 
 struct AuthenticationButtonType: ButtonStyle {
     @Environment(\.isEnabled) private var isEnabled
+    @Environment(\.colorScheme) private var colorScheme
+    
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .frame(maxWidth: .infinity)
             .padding(.vertical)
             .foregroundStyle(Color.white)
             .font(.system(size: 20, weight: .bold, design: .rounded))
-            .background(Color.black)
-            .cornerRadius(15)
+            .background(colorScheme == .dark ? Color(UIColor.systemGray5) : Color(UIColor.black))
+            .cornerRadius(20)
             .brightness(configuration.isPressed ? 0.05 : 0)
             .opacity(isEnabled ? (configuration.isPressed ? 0.7 : 1) : 0.4)
             .padding(.vertical, 12)
@@ -199,16 +201,16 @@ struct StudentAuthenticationTextFieldStyle: TextFieldStyle {
         configuration
             .padding(.horizontal, 20)
             .padding(.vertical, 10)
-            .font(.system(size: 20, weight: .regular, design: .rounded))
+            .font(.system(size: 18, weight: .regular, design: .rounded))
             .background(
                 ZStack {
-                    RoundedRectangle(cornerRadius: 16, style: .continuous)
+                    RoundedRectangle(cornerRadius: 20, style: .continuous)
                         .stroke(isFocused.wrappedValue ? Color.white : Color.white, lineWidth: 1)
-                        .stroke(Color.black, lineWidth: 1)
+                        .stroke(colorScheme == .light ? Color.black : Color.white, lineWidth: 1)
                         .zIndex(1)
-                    RoundedRectangle(cornerRadius: 16)
+                    RoundedRectangle(cornerRadius: 20)
                         .fill(colorScheme == .light ? Color.white : Color.black)
-                        .stroke(Color.black, lineWidth: 1)
+                        .stroke(colorScheme == .light ? Color.black : Color.white, lineWidth: 1)
                         .zIndex(0)
                 }
             )
@@ -329,7 +331,7 @@ struct BottomView: View {
                 } label: {
                     Text(authenticationType == .login ? "Register" : "Login")
                         .font(Font.system(size: 15, weight: .bold, design: .rounded))
-                        .foregroundColor(.black)
+                        .foregroundColor(colorScheme == .light ? .black : .white)
                 }
 
             }
@@ -393,6 +395,7 @@ struct BottomView: View {
     let preview = PersistenceController.preview
     return NavigationStack {
         StudentAuthenticationView()
+            .preferredColorScheme(.light)
             .environmentObject(StudentAuthViewModel(context: preview.container.viewContext))
     }
 }
