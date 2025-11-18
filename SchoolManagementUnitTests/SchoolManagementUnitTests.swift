c9c3a855928a54c842b5ea59057f64c4a23d6ddb
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2025-11-18 20:53:58 +0530

    Unit tests target added

---
+++ SchoolManagementUnitTests/SchoolManagementUnitTests.swift
@@ -0,0 +1,16 @@
+//
+//  SchoolManagementUnitTests.swift
+//  SchoolManagementUnitTests
+//
+//  Created by Sajed Shaikh on 18/11/25.
+//
+
+import Testing
+
+struct SchoolManagementUnitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
